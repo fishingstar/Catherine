5597d0f5d0ac3736572b051462cdd073323241e7
liujunda <446566706@qq.com>
2019-10-03 09:57:30 +0800

    fix matrix transform bug

--- Src/RenderContext.cpp
+++ Src/RenderContext.cpp
@@ -6,16 +6,15 @@ namespace Catherine
 	void RenderContext::Apply()
 	{
 		m_ModelMatrix = glm::mat4x4(1.0f);
-
-		// 1st scale
-		m_ModelMatrix = glm::scale(m_ModelMatrix, m_Scale);
-
-		// 2nd rotation
-		m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(m_Rotation.x), glm::vec3(-1.0f, 0.0f, 0.0f));
-		m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(m_Rotation.y), glm::vec3(0.0f, -1.0f, 0.0f));
-		m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(m_Rotation.z), glm::vec3(0.0f, 0.0f, -1.0f));
-		
 		// 3rd position
 		m_ModelMatrix = glm::translate(m_ModelMatrix, m_Position);
+		
+		// 2nd rotation
+		m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(m_Rotation.z), glm::vec3(0.0f, 0.0f, -1.0f));
+		m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(m_Rotation.y), glm::vec3(0.0f, -1.0f, 0.0f));
+		m_ModelMatrix = glm::rotate(m_ModelMatrix, glm::radians(m_Rotation.x), glm::vec3(-1.0f, 0.0f, 0.0f));
+		
+		// 1st scale
+		m_ModelMatrix = glm::scale(m_ModelMatrix, m_Scale);
 	}
 }
